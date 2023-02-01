11b9fd1612e2c381e721f7ff2c6a0230751396fb
cloudwi <cloudwi@naver.com>
2023-02-01 21:50:38 +0900

    Initial commit

---
+++ src/main/java/com/project/cafearound/CafeAroundApplication.java
@@ -0,0 +1,13 @@
+package com.project.cafearound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeAroundApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CafeAroundApplication.class, args);
+  }
+
+}
